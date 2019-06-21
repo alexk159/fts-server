b7525ce1351dcb839343e053591444b08fbcd1cd
akiryushkin <kiryushkin-ay@bisys.ru>
2019-06-21 12:10:52 +0500

    fixes

--- core/src/main/kotlin/com/akiryushkin/ftsserver/service/PythonService.kt
+++ core/src/main/kotlin/com/akiryushkin/ftsserver/service/PythonService.kt
@@ -3,14 +3,14 @@ package com.akiryushkin.ftsserver.service;
 import com.akiryushkin.ftsserver.configuration.PythonRunnerProperties
 import com.akiryushkin.ftsserver.data.entity.ScriptData
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.stereotype.Service;
-import java.io.*
+import org.springframework.stereotype.Service
 import java.io.BufferedReader
-import java.io.FileWriter
-import java.io.BufferedWriter
+import java.io.File
+import java.io.InputStreamReader
 import java.util.*
 import javax.persistence.EntityManager
 import javax.persistence.PersistenceContext
+import javax.transaction.Transactional
 
 
 @Service
@@ -37,24 +37,23 @@ class PythonService {
         em.flush()
     }
 
+    @Transactional
     fun runScript(scriptName: String, funcName: String): String? {
         val dir = File(pythonRunnerProperties.scriptsPath)
-        val p = Runtime.getRuntime().exec("python -c 'from $scriptName import *; print $funcName()'",
+        val p = Runtime.getRuntime().exec(arrayOf<String>("python", "-c", "from $scriptName import *; print $funcName()"),
                 null, dir)
         val `in` = BufferedReader(InputStreamReader(p.inputStream))
         val  in1 = BufferedReader(InputStreamReader(p.errorStream))
 
-        val ret1 = in1.readLine()
-        val ret2 = in1.readLine()
-        val ret3 = in1.readLine()
-        val ret4 = in1.readLine()
-        val ret5 = in1.readLine()
-        val ret = `in`.readLine()
+        var res = ""
+        while (true) {
+            val tmp = `in`.readLine()
+            if (tmp != null) res += tmp else break;
+        }
 
-        println(ret1 + ret2 + ret3 +ret4 + ret5)
         val uuid = UUID.randomUUID().toString()
         val scriptData = ScriptData()
-        scriptData.data = "test"
+        scriptData.data = res
         scriptData.uuid = uuid
         em.persist(scriptData)
         em.flush()
